95c83ed3af30d21733c8dd376af72f942b193681
nikhilgaikwad9540 <143881200+nikhilgaikwad9540@users.noreply.github.com>
2024-07-22 13:27:00 +0530

    Add files via upload

---
+++ navbar.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        document.querySelector('.hero-container').classList.add('visible');
+    }, 1000); 
+});
+
+document.addEventListener("DOMContentLoaded",function(){
+    setTimeout(function() {
+        document.querySelector('.hero-container2').classList.add('visible');
+    }, 5000); 
+});
